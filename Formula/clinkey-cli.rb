d71a1ab807d64123f9675bdb6f4b2c0019995a35
dim-gggl <dimitri.gaggioli@gmail.com>
2025-09-29 12:30:37 +0200

    bug fixed

--- Formula/clinkey-cli.rb
+++ Formula/clinkey-cli.rb
@@ -9,7 +9,7 @@ class ClinkeyCli < Formula
 	url "https://github.com/dim-gggl/clinkey-cli/archive/refs/tags/v1.0.1.tar.gz"
   
 	# Le hash de sécurité que vous avez calculé
-	sha256 "c3022aae431d6978b7895768a65da13dbc2d6aace4ce7a2dbf748e83dc0c207f"
+	sha256 "3d9715a8d066d75636b296532d6b8ba53304e7b4ed6e41cb78f7e09d7eec0a64"
   
 	depends_on "python@3.12"
   
